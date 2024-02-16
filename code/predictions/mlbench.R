242fe53f1438dfe27ccfe8d5a8caa1962e142a0f
Timo Koch <timokoch@Timos-Air.fritz.box>
2024-02-16 11:05:27 +0100

    merged all benchmark code into one script

--- code/predictions/mlbench.R
+++ code/predictions/mlbench.R
@@ -97,63 +97,38 @@ compare_arousal = TaskRegr$new(id = "compare_arousal",
 ## add blocking
 
 # age
-# Use participant id column as block factor
 egemaps_age$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_age$col_roles$feature = setdiff(egemaps_age$col_roles$feature, "user_id")
 
 # gender
-# Use participant id column as block factor
 egemaps_gender$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_gender$col_roles$feature = setdiff(egemaps_gender$col_roles$feature, "user_id")
 
 # raw valence score
-# Use participant id column as block factor
 egemaps_valence$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_valence$col_roles$feature = setdiff(egemaps_valence$col_roles$feature, "user_id")
 
 # raw arousal score
-# Use Id column as block factor
 egemaps_arousal$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_arousal$col_roles$feature = setdiff(egemaps_arousal$col_roles$feature, "user_id")
 
 # valence difference from baseline
-# Use participant id column as block factor
 egemaps_valence_diff$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_valence_diff$col_roles$feature = setdiff(egemaps_valence_diff$col_roles$feature, "user_id")
 
 # arousal difference from baseline
-# Use Id column as block factor
 egemaps_arousal_diff$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_arousal_diff$col_roles$feature = setdiff(egemaps_arousal_diff$col_roles$feature, "user_id")
 
-
-# valence
-# Use participant id column as block factor
+# valence, compare feature set
 compare_valence$col_roles$group = "user_id"
-
-# Remove Id from feature space
 compare_valence$col_roles$feature = setdiff(compare_valence$col_roles$feature, "user_id")
 
-# arousal
+# arousal, compare feature set
 
 # Use Id column as block factor
 compare_arousal$col_roles$group = "user_id"
-
-# Remove Id from feature space
 compare_arousal$col_roles$feature = setdiff(compare_arousal$col_roles$feature, "user_id")
-
 
 #### CREATE TASKS: STUDY 2 ####
 
@@ -221,66 +196,129 @@ egemaps_arousal_diff = TaskRegr$new(id = "egemaps_arousal_diff",
                                                                which(colnames(affect_acoustics)=="F0semitoneFrom27.5Hz_sma3nz_amean"):which(colnames(affect_acoustics)=="equivalentSoundLevel_dBp"))], 
                                 target = "diff_arousal")
 
-# remove data set from working memory to clear up memory
-rm(affect_acoustics)
+## supplementary analyses: compare feature set
+
+# raw contentedness score
+compare_content = TaskRegr$new(id = "compare_content", 
+                               backend = ema_acoustics_data[,c(which(colnames(ema_acoustics_data)=="user_id"), which(colnames(ema_acoustics_data)== "content"), which(colnames(ema_acoustics_data)=="audspec_lengthL1norm_sma_range"):which(colnames(ema_acoustics_data)=="mfcc_sma_de.14._stddevFallingSlope"))], 
+                               target = "content")
+
+# raw sadness score
+compare_sad = TaskRegr$new(id = "compare_sad", 
+                           backend = ema_acoustics_data[,c(which(colnames(ema_acoustics_data)=="user_id"), which(colnames(ema_acoustics_data)== "sad"), which(colnames(ema_acoustics_data)== "sad_weight"), which(colnames(ema_acoustics_data)=="audspec_lengthL1norm_sma_range"):which(colnames(ema_acoustics_data)=="mfcc_sma_de.14._stddevFallingSlope"))], 
+                           target = "sad")
+
+# raw arousal score
+compare_arousal = TaskRegr$new(id = "compare_arousal", 
+                               backend = ema_acoustics_data[,c(which(colnames(ema_acoustics_data)=="user_id"), which(colnames(ema_acoustics_data)== "arousal"), which(colnames(ema_acoustics_data)=="audspec_lengthL1norm_sma_range"):which(colnames(ema_acoustics_data)=="mfcc_sma_de.14._stddevFallingSlope"))], 
+                               target = "arousal")
+
+## word embeddings 
+
+# raw contentedness score
+wordembeddings_content = TaskRegr$new(id = "wordembeddings_content", 
+                                      backend = affect_wordembeddings[,c(which(colnames(affect_wordembeddings)=="user_id"), which(colnames(affect_wordembeddings)=="content"), which(colnames(affect_wordembeddings)=="Dim1"):which(colnames(affect_wordembeddings)=="Dim1024"))], 
+                                      target = "content")
+
+# raw sadness score
+wordembeddings_sad = TaskRegr$new(id = "wordembeddings_sad", 
+                                  backend = affect_wordembeddings[,c(which(colnames(affect_wordembeddings)=="user_id"), which(colnames(affect_wordembeddings)=="sad"), which(colnames(affect_wordembeddings)=="Dim1"):which(colnames(affect_wordembeddings)=="Dim1024"))], 
+                                  target = "sad")
+
+# raw arousal score
+wordembeddings_arousal = TaskRegr$new(id = "wordembeddings_arousal", 
+                                      backend = affect_wordembeddings[,c(which(colnames(affect_wordembeddings)=="user_id"), which(colnames(affect_wordembeddings)=="arousal"), which(colnames(affect_wordembeddings)=="Dim1"):which(colnames(affect_wordembeddings)=="Dim1024"))], 
+                                      target = "arousal")
+
+# supplementary analyses
+
+# content fluctuation from baseline
+wordembeddings_content_diff = TaskRegr$new(id = "wordembeddings_content_diff", 
+                                           backend = affect_wordembeddings[,c(which(colnames(affect_wordembeddings)=="user_id"), which(colnames(affect_wordembeddings)=="diff_content"), which(colnames(affect_wordembeddings)=="Dim1"):which(colnames(affect_wordembeddings)=="Dim1024"))], 
+                                           target = "diff_content")
+
+# sad fluctuation from baseline
+wordembeddings_sad_diff = TaskRegr$new(id = "wordembeddings_sad_diff", 
+                                       backend = affect_wordembeddings[,c(which(colnames(affect_wordembeddings)=="user_id"), which(colnames(affect_wordembeddings)=="diff_sad"), which(colnames(affect_wordembeddings)=="Dim1"):which(colnames(affect_wordembeddings)=="Dim1024"))], 
+                                       target = "diff_sad")
+
+# arousal fluctuation from baseline
+wordembeddings_arousal_diff = TaskRegr$new(id = "wordembeddings_arousal_diff", 
+                                           backend = affect_wordembeddings[,c(which(colnames(affect_wordembeddings)=="user_id"), which(colnames(affect_wordembeddings)=="diff_arousal"), which(colnames(affect_wordembeddings)=="Dim1"):which(colnames(affect_wordembeddings)=="Dim1024"))], 
+                                           target = "diff_arousal")
+
 
 ## add blocking
 
 # age
-# Use participant id column as block factor
 egemaps_age$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_age$col_roles$feature = setdiff(egemaps_age$col_roles$feature, "user_id")
 
 # gender
-# Use participant id column as block factor
 egemaps_gender$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_gender$col_roles$feature = setdiff(egemaps_gender$col_roles$feature, "user_id")
 
 # raw content score
-# Use participant id column as block factor
 egemaps_content$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_content$col_roles$feature = setdiff(egemaps_content$col_roles$feature, "user_id")
 
 # raw sad score
-# Use Id column as block factor
 egemaps_sad$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_sad$col_roles$feature = setdiff(egemaps_sad$col_roles$feature, "user_id")
 
 # raw arousal score
-# Use Id column as block factor
 egemaps_arousal$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_arousal$col_roles$feature = setdiff(egemaps_arousal$col_roles$feature, "user_id")
 
 # content difference from baseline
-# Use participant id column as block factor
 egemaps_content_diff$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_content_diff$col_roles$feature = setdiff(egemaps_content_diff$col_roles$feature, "user_id")
 
 # sad difference from baseline
-# Use Id column as block factor
 egemaps_sad_diff$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_sad_diff$col_roles$feature = setdiff(egemaps_sad_diff$col_roles$feature, "user_id")
 
 # arousal difference from baseline
-# Use Id column as block factor
 egemaps_arousal_diff$col_roles$group = "user_id"
-
-# Remove Id from feature space
 egemaps_arousal_diff$col_roles$feature = setdiff(egemaps_arousal_diff$col_roles$feature, "user_id")
+
+# raw content score, compare feature set
+compare_content$col_roles$group = "user_id"
+compare_content$col_roles$feature = setdiff(compare_content$col_roles$feature, "user_id")
+
+# raw sad score, compare feature set
+compare_sad$col_roles$group = "user_id"
+compare_sad$col_roles$feature = setdiff(compare_sad$col_roles$feature, "user_id")
+
+# raw arousal score, compare feature set
+compare_arousal$col_roles$group = "user_id"
+compare_arousal$col_roles$feature = setdiff(compare_arousal$col_roles$feature, "user_id")
+
+# word embeddings 
+
+# raw content 
+wordembeddings_content$col_roles$group = "user_id"
+wordembeddings_content$col_roles$feature = setdiff(wordembeddings_content$col_roles$feature, "user_id")
+
+# raw sad score
+wordembeddings_sad$col_roles$group = "user_id"
+wordembeddings_sad$col_roles$feature = setdiff(wordembeddings_sad$col_roles$feature, "user_id")
+
+# raw arousal score
+wordembeddings_arousal$col_roles$group = "user_id"
+wordembeddings_arousal$col_roles$feature = setdiff(wordembeddings_arousal$col_roles$feature, "user_id")
+
+# content difference from baseline
+wordembeddings_content_diff$col_roles$group = "user_id"
+wordembeddings_content_diff$col_roles$feature = setdiff(wordembeddings_content_diff$col_roles$feature, "user_id")
+
+# sad difference from baseline
+wordembeddings_sad_diff$col_roles$group = "user_id"
+wordembeddings_sad_diff$col_roles$feature = setdiff(wordembeddings_sad_diff$col_roles$feature, "user_id")
+
+# arousal difference from baseline
+wordembeddings_arousal_diff$col_roles$group = "user_id"
+wordembeddings_arousal_diff$col_roles$feature = setdiff(wordembeddings_arousal_diff$col_roles$feature, "user_id")
 
 #### CREATE LEARNERS ####
 
@@ -321,6 +359,14 @@ at_rr = AutoTuner$new(
 resampling = rsmp("cv", folds = 10L)
 
 #### BENCHMARK: STUDY 1 ####
+
+# avoid console output from mlr3tuning
+logger = lgr::get_logger("bbotk")
+logger$set_threshold("warn")
+
+# show progress
+progressr::handlers(global = TRUE)
+progressr::handlers("progress")
 
 # age
 bmgrid_egemaps_age = benchmark_grid(
@@ -376,17 +422,17 @@ lrn_rf_at_po = po_preproc %>>% at_rf
 lrn_rr_at_po = po_preproc %>>% at_rr 
 
 bmgrid_compare = benchmark_grid(
-  task = c(compare_valence, compare_arousal),
+  task = c(compare_valence, 
+           compare_arousal),
   learner = list(lrn_fl, lrn_rf_at_po,  lrn_rr_at_po),
   resampling = resampling
 )
 
-future::plan("multisession", workers = detectCores()) # enable parallelization
+future::plan("multisession", workers = 10) # enable parallelization
 
 bmr_compare_valence = benchmark(bmgrid_compare_valence, store_models = F, store_backends = F) # execute the benchmark
 
 saveRDS(bmr_compare_valence, "results/study1/bmr_compare_valence.RData") # save results
-
 
 
 #### BENCHMARK: STUDY 2 ####
@@ -457,11 +503,28 @@ bmgrid_compare = benchmark_grid(
   resampling = resampling
 )
 
-future::plan("multisession", workers = detectCores()) # enable parallelization
+future::plan("multisession", workers = 10) # enable parallelization
 
-bmr_compare_valence = benchmark(bmgrid_compare_valence, store_models = F, store_backends = F) # execute the benchmark
+bmr_compare = benchmark(bmgrid_compare, store_models = F, store_backends = F) # execute the benchmark
 
-saveRDS(bmr_compare_valence, "study1_ger/results/bmr_compare_valence.RData") # save results
+saveRDS(bmr_compare_valence, "results/study2/bmr_compare.RData") # save results
+
+# word embeddings 
+
+bmgrid_wordembeddings = benchmark_grid(
+  task = c(wordembeddings_arousal,
+           wordembeddings_content,
+           wordembeddings_sad
+  ),
+  learner = list(lrn_fl, at_rf, at_rr),
+  resampling = resampling
+)
+
+future::plan("multisession", workers = 10) # enable parallelization
+
+bmr_wordembeddings = benchmark(bmgrid_wordembeddings, store_models = F, store_backends = F) # execute the benchmark
+
+saveRDS(bmr_wordembeddings, "results/study2/bmr_wordembeddings.RData") # save results
 
 
 #### BENCHMARK RESULTS AND SIGNIFICANCE TESTS ####
@@ -535,5 +598,89 @@ png(file="figures/bmr_ger_egemaps_plot.png",width=1250, height=750)
 bmr_egemaps_plot
 
 dev.off()
+
+# add column with p values
+bmr_results_folds <- dplyr::left_join(bmr_results_folds, pred_table[,c("task_id", "learner_id", "p_rsq", "p_rsq_corrected")], by = c("task_id", "learner_id"))
+
+# create significance column
+bmr_results_folds$significance <- as.factor(ifelse(bmr_results_folds$p_rsq_corrected >= 0.05 | is.na(bmr_results_folds$p_rsq_corrected), "no", "yes"))
+
+# rename 
+bmr_results_folds <- bmr_results_folds %>% 
+  mutate(learner_id = case_when(
+    learner_id == "regr.featureless" ~    "Baseline",
+    learner_id == "regr.ranger" ~ "Random Forest",
+    learner_id == "regr.cv_glmnet" ~ "LASSO")) %>% 
+  mutate(feature_set = case_when(
+    task_id == "egemaps_content" ~    "Voice Acoustics",
+    task_id == "egemaps_content_diff" ~ "Voice Acoustics",
+    task_id == "egemaps_sad" ~    "Voice Acoustics",
+    task_id == "egemaps_sad_diff" ~ "Voice Acoustics",
+    task_id == "egemaps_arousal" ~ "Voice Acoustics",
+    task_id == "egemaps_arousal_diff" ~ "Voice Acoustics",
+    task_id == "wordembeddings_content" ~    "Word Embeddings",
+    task_id == "wordembeddings_content_diff" ~ "Word Embeddings",
+    task_id == "wordembeddings_sad" ~    "Word Embeddings",
+    task_id == "wordembeddings_sad_diff" ~ "Word Embeddings",
+    task_id == "wordembeddings_arousal" ~ "Word Embeddings",
+    task_id == "wordembeddings_arousal_diff" ~ "Word Embeddings",
+    task_id == "egemaps_wordembeddings_content" ~    "All Features",
+    task_id == "egemaps_wordembeddings_content_diff" ~ "All Features",
+    task_id == "egemaps_wordembeddings_sad" ~    "All Features",
+    task_id == "egemaps_wordembeddings_sad_diff" ~ "All Features",
+    task_id == "egemaps_wordembeddings_arousal" ~ "All Features",
+    task_id == "egemaps_wordembeddings_arousal_diff" ~ "All Features")) %>% 
+  mutate(task_id = case_when(
+    task_id == "egemaps_content" ~    "Contentedness",
+    task_id == "egemaps_content_diff" ~ "Contentedness Fluctuation",
+    task_id == "egemaps_sad" ~    "Sadness",
+    task_id == "egemaps_sad_diff" ~ "Sadness Fluctuation",
+    task_id == "egemaps_arousal" ~ "Arousal",
+    task_id == "egemaps_arousal_diff" ~ "Arousal Fluctuation",
+    task_id == "wordembeddings_content" ~    "Contentedness",
+    task_id == "wordembeddings_content_diff" ~ "Contentedness Fluctuation",
+    task_id == "wordembeddings_sad" ~    "Sadness",
+    task_id == "wordembeddings_sad_diff" ~ "Sadness Fluctuation",
+    task_id == "wordembeddings_arousal" ~ "Arousal",
+    task_id == "wordembeddings_arousal_diff" ~ "Arousal Fluctuation",
+    task_id == "egemaps_wordembeddings_content" ~    "Contentedness",
+    task_id == "egemaps_wordembeddings_content_diff" ~ "Contentedness Fluctuation",
+    task_id == "egemaps_wordembeddings_sad" ~    "Sadness",
+    task_id == "egemaps_wordembeddings_sad_diff" ~ "Sadness Fluctuation",
+    task_id == "egemaps_wordembeddings_arousal" ~ "Arousal",
+    task_id == "egemaps_wordembeddings_arousal_diff" ~ "Arousal Fluctuation")) 
+
+# create figure
+
+bmr_plot <- ggplot(bmr_results_folds, aes(x= factor(interaction(task_id, feature_set),
+                                                    levels = c("Arousal Fluctuation.Voice Acoustics","Arousal.Voice Acoustics", "Sadness Fluctuation.Voice Acoustics",  "Sadness.Voice Acoustics", "Contentedness Fluctuation.Voice Acoustics",  "Contentedness.Voice Acoustics",
+                                                               "Arousal Fluctuation.Word Embeddings","Arousal.Word Embeddings", "Sadness Fluctuation.Word Embeddings",  "Sadness.Word Embeddings", "Contentedness Fluctuation.Word Embeddings",  "Contentedness.Word Embeddings",
+                                                               "Arousal Fluctuation.All Features","Arousal.All Features", "Sadness Fluctuation.All Features",  "Sadness.All Features", "Contentedness Fluctuation.All Features",  "Contentedness.All Features")) , 
+                                          y= regr.rsq, 
+                                          color = significance, 
+                                          shape = learner_id)) + 
+  geom_boxplot(width = 0.3,lwd = 1, aes(color = significance), alpha = 0.3, outlier.shape=NA, position=position_dodge(0.5)) +  
+  geom_point(position=position_jitterdodge(jitter.width = 0.1, dodge.width = 0.5), size = 3) +
+  scale_x_discrete(element_blank(), labels = c("Arousal Fluctuation (Voice Acoustics)","Arousal (Voice Acoustics)", "Sadness Fluctuation (Voice Acoustics)",  "Sadness (Voice Acoustics)", "Contentedness Fluctuation (Voice Acoustics)",  "Contentedness (Voice Acoustics)",
+                                               "Arousal Fluctuation (Word Embeddings)","Arousal (Word Embeddings)", "Sadness Fluctuation (Word Embeddings)",  "Sadness (Word Embeddings)", "Contentedness Fluctuation (Word Embeddings)",  "Contentedness (Word Embeddings)",
+                                               "Arousal Fluctuation (All Features)","Arousal (All Features)", "Sadness Fluctuation (All Features)",  "Sadness (All Features)", "Contentedness Fluctuation (All Features)",  "Contentedness (All Features)")) +
+  scale_y_continuous(name = bquote(paste("Out-of-sample ", "R"^2)), limits = c(-0.15, 0.15)) + 
+  geom_hline(yintercept=0, linetype='dotted') +
+  theme_minimal(base_size = 25) +
+  labs(colour = "Significance", shape = "Algorithm") + # change legend title
+  theme(axis.text.x=element_text(angle = -45, hjust = 0)) + # rotate x axis labels
+  coord_flip() + # flip coordinates
+  guides(color = guide_legend(reverse = TRUE), shape = guide_legend(reverse = TRUE)) +
+  theme(legend.position="top", legend.key.size = unit(1, "cm"))
+
+# save figure
+
+png(file="figures/bmr_us_egemaps_wordembeddings_plot.png",width=1500, height=2000)
+
+bmr_plot
+
+dev.off()
+
+
 
 ### FINISH
