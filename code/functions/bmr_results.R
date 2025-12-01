4a196b66f4a116e6e5d50c2b2eeed2e20a2585b9
Timo-Ko <you@example.com>
2025-12-01 15:10:03 +0000

    updated helper funtion to create prediction results table

--- code/functions/bmr_results.R
+++ code/functions/bmr_results.R
@@ -1,27 +1,54 @@
 #' @param bmr a mlr3 benchmark object
 #' @param mes performance measures
 #' @return df_results - a data frame with the perfromance measures per fold
+# 
+# extract_bmr_results = function(bmr, mes){
+#   
+#   df = as.data.frame(bmr$aggregate(mes)) # get aggregated performance across folds
+#   uni.learner = unique(df$learner_id) # get unique learner ids
+#   uni.task = unique(df$task_id) # get unique task ids
+#   df_results = data.frame() # create empty df with results
+#   
+#   for(i in 1:length(uni.task)){ #iterate through task ids
+#     
+#     ## standard fl, elastic net, rf
+#     df_helper = as.data.frame(bmr$aggregate(mes)[learner_id == uni.learner[1]]$resample_result[[i]]$score(mes)) # fl
+#     df_helper = rbind(df_helper, bmr$aggregate(mes)[learner_id == uni.learner[2]]$resample_result[[i]]$score(mes)) # elastic net
+#     df_helper = rbind(df_helper, bmr$aggregate(mes)[learner_id == uni.learner[3]]$resample_result[[i]]$score(mes)) # rf
+#     
+#     df_results = rbind(df_results, df_helper)
+#     
+#     }
+#   
+#   return(df_results) # return df results data frame 
+# }
 
-extract_bmr_results = function(bmr, mes){
+extract_bmr_results <- function(bmr, measures) {
+  library(data.table)
   
-  df = as.data.frame(bmr$aggregate(mes)) # get aggregated performance across folds
-  uni.learner = unique(df$learner_id) # get unique learner ids
-  uni.task = unique(df$task_id) # get unique task ids
-  df_results = data.frame() # create empty df with results
+  # convert aggregated benchmark object to a data.table
+  agg <- as.data.table(bmr$aggregate(measures))
   
-  for(i in 1:length(uni.task)){ #iterate through task ids
-    
-    ## standard fl, elastic net, rf
-    df_helper = as.data.frame(bmr$aggregate(mes)[learner_id == uni.learner[1]]$resample_result[[i]]$score(mes)) # fl
-    df_helper = rbind(df_helper, bmr$aggregate(mes)[learner_id == uni.learner[2]]$resample_result[[i]]$score(mes)) # elastic net
-    df_helper = rbind(df_helper, bmr$aggregate(mes)[learner_id == uni.learner[3]]$resample_result[[i]]$score(mes)) # rf
-    
-    df_results = rbind(df_results, df_helper)
-    
-    }
+  # prepare results collector
+  all_res <- list()
   
-  return(df_results) # return df results data frame 
+  for (i in seq_len(nrow(agg))) {
+    task_id    <- agg$task_id[i]
+    learner_id <- agg$learner_id[i]
+    rr         <- agg$resample_result[[i]]  # the ResampleResult object
+    
+    # extract *fold-level* performance
+    sc <- as.data.table(rr$score(measures))
+    sc[, task_id := task_id]
+    sc[, learner_id := learner_id]
+    
+    all_res[[length(all_res) + 1]] <- sc
+  }
+  
+  rbindlist(all_res, use.names = TRUE, fill = TRUE)
 }
+
+
 
 
 #' @param data the data frame the ml algos had been trained and tested on
@@ -112,10 +139,46 @@ extract_bmr_results = function(bmr, mes){
 #   
 #   return(results.table)
 # }
-
-
 results_table <- function(data, bmr_results) {
   library(dplyr)
+  
+  # desired learner order
+  learner_order <- c("regr.featureless", "regr.cv_glmnet", "en_pca100", "regr.ranger", "rf_pca100")
+  
+  # desired task order (exactly in the order you want in the table)
+  task_order <- c(
+    "ensemble_content",
+    "wordembeddings_content",
+    "wordembeddings_masked_content",
+    "speechembeddings_content",
+    "liwc_content",
+    "liwc_masked_content",
+    "egemaps_content",
+    "ensemble_sad",
+    "wordembeddings_sad",
+    "wordembeddings_masked_sad",
+    "speechembeddings_sad",
+    "liwc_sad",
+    "liwc_masked_sad",
+    "egemaps_sad",
+    "ensemble_arousal",
+    "wordembeddings_arousal",
+    "wordembeddings_masked_arousal",
+    "speechembeddings_arousal",
+    "liwc_arousal",
+    "liwc_masked_arousal",
+    "egemaps_arousal"
+  )
+  
+  # helper: map task_id to target label
+  label_target_from_task <- function(x) {
+    case_when(
+      grepl("content", x, ignore.case = TRUE) ~ "Contentment",
+      grepl("sad",     x, ignore.case = TRUE) ~ "Sadness",
+      grepl("arousal|energy", x, ignore.case = TRUE) ~ "Arousal",
+      TRUE ~ "Other"
+    )
+  }
   
   out <- bmr_results %>%
     group_by(task_id, learner_id) %>%
@@ -124,27 +187,35 @@ results_table <- function(data, bmr_results) {
       Md_srho  = median(regr.srho,  na.rm = TRUE),
       LCI_srho = quantile(regr.srho, 0.025, na.rm = TRUE),
       UCI_srho = quantile(regr.srho, 0.975, na.rm = TRUE),
-      
       # R^2
       Md_rsq  = median(regr.rsq,  na.rm = TRUE),
       LCI_rsq = quantile(regr.rsq, 0.025, na.rm = TRUE),
       UCI_rsq = quantile(regr.rsq, 0.975, na.rm = TRUE),
-      
       # MAE
       Md_mae  = median(regr.mae,  na.rm = TRUE),
       LCI_mae = quantile(regr.mae, 0.025, na.rm = TRUE),
       UCI_mae = quantile(regr.mae, 0.975, na.rm = TRUE),
-      
       # RMSE
       Md_rmse  = median(regr.rmse,  na.rm = TRUE),
       LCI_rmse = quantile(regr.rmse, 0.025, na.rm = TRUE),
       UCI_rmse = quantile(regr.rmse, 0.975, na.rm = TRUE),
       .groups = "drop"
     ) %>%
-    arrange(task_id, learner_id) %>%
-    mutate(across(where(is.numeric), ~ round(., 3)))
+    mutate(
+      target    = label_target_from_task(task_id),
+      target    = factor(target, levels = c("Contentment", "Sadness", "Arousal")),
+      task_id   = factor(task_id, levels = task_order),
+      learner_id = factor(learner_id, levels = learner_order)
+    ) %>%
+    arrange(target, task_id, learner_id) %>%
+    mutate(
+      srho = sprintf("%.3f [%.3f, %.3f]", Md_srho, LCI_srho, UCI_srho),
+      rsq  = sprintf("%.3f [%.3f, %.3f]", Md_rsq,  LCI_rsq,  UCI_rsq),
+      mae  = sprintf("%.3f [%.3f, %.3f]", Md_mae,  LCI_mae,  UCI_mae),
+      rmse = sprintf("%.3f [%.3f, %.3f]", Md_rmse, LCI_rmse, UCI_rmse)
+    ) %>%
+    select(target, task_id, learner_id, srho, rsq, mae, rmse)
   
-  return(out)
+  out
 }
-
 
